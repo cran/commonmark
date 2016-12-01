8d7e4513f6bc1ec8def633422d28e4479d704609
Jeroen Ooms <jeroen.ooms@stat.ucla.edu>
2016-12-01 18:44:30 +0000

    version 1.0

--- R/markdown.R
+++ R/markdown.R
@@ -1,11 +1,14 @@
 #' Parse and render markdown text
 #'
-#' Converts markdown text to several formats using John MacFarlane's \href{https://github.com/jgm/cmark}{cmark}
+#' Converts markdown text to several formats using John MacFarlane's [cmark](https://github.com/jgm/cmark)
 #' reference implementation. Output in HTML, groff man, CommonMark, and a custom XML format is supported.
 #'
+#' Support for extensions is provided via the Github [fork](https://github.com/github/cmark) of cmark.
+#' See also the manual page on [extensions].
+#'
 #' When smart punctuation is enabled, straight double and single quotes will be rendered as curly quotes,
-#' depending on their position. Moreover \code{--} will be rendered as an en-dash, \code{---} will be
-#' rendered as an em-dash, and \code{...} will be rendered as ellipses.
+#' depending on their position. Moreover `--` will be rendered as an en-dash, `---`` will be
+#' rendered as an em-dash, and `...` will be rendered as ellipses.
 #'
 #' @useDynLib commonmark R_render_markdown
 #' @aliases commonmark markdown
@@ -15,9 +18,11 @@
 #' @param text Markdown text
 #' @param sourcepos Include source position attribute in output.
 #' @param hardbreaks Treat newlines as hard line breaks. If this option is specified, hard wrapping is disabled
-#' regardless of the value given with \code{width}.
+#' regardless of the value given with `width`.
 #' @param smart Use smart punctuation. See details.
 #' @param normalize Consolidate adjacent text nodes.
+#' @param extensions Enables Github extensions. Can be `TRUE` (all) `FALSE` (none) or a character
+#' vector with a subset of available [extensions].
 #' @param width Specify wrap width (default 0 = nowrap).
 #' @examples md <- readLines(curl::curl("https://raw.githubusercontent.com/yihui/knitr/master/NEWS.md"))
 #' html <- markdown_html(md)
@@ -25,35 +30,40 @@
 #' man <- markdown_man(md)
 #' tex <- markdown_latex(md)
 #' cm <- markdown_commonmark(md)
-markdown_html <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, sourcepos = FALSE){
+markdown_html <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, sourcepos = FALSE, extensions = FALSE){
   text <- enc2utf8(paste(text, collapse="\n"))
-  .Call(R_render_markdown, text, 1L, sourcepos, hardbreaks, smart, normalize, 0L)
+  extensions <- get_extensions(extensions)
+  .Call(R_render_markdown, text, 1L, sourcepos, hardbreaks, smart, normalize, 0L, extensions)
 }
 
 #' @export
 #' @rdname commonmark
-markdown_xml <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, sourcepos = FALSE){
+markdown_xml <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, sourcepos = FALSE, extensions = FALSE){
   text <- enc2utf8(paste(text, collapse="\n"))
-  .Call(R_render_markdown, text, 2L, sourcepos, hardbreaks, smart, normalize, 0L)
+  extensions <- get_extensions(extensions)
+  .Call(R_render_markdown, text, 2L, sourcepos, hardbreaks, smart, normalize, 0L, extensions)
 }
 
 #' @export
 #' @rdname commonmark
-markdown_man <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, width = 0){
+markdown_man <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, width = 0, extensions = FALSE){
   text <- enc2utf8(paste(text, collapse="\n"))
-  .Call(R_render_markdown, text, 3L, FALSE, hardbreaks, smart, normalize, as.integer(width))
+  extensions <- get_extensions(extensions)
+  .Call(R_render_markdown, text, 3L, FALSE, hardbreaks, smart, normalize, as.integer(width), extensions)
 }
 
 #' @export
 #' @rdname commonmark
-markdown_commonmark <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, width = 0){
+markdown_commonmark <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, width = 0, extensions = FALSE){
   text <- enc2utf8(paste(text, collapse="\n"))
-  .Call(R_render_markdown, text, 4L, FALSE, hardbreaks, smart, normalize, as.integer(width))
+  extensions <- get_extensions(extensions)
+  .Call(R_render_markdown, text, 4L, FALSE, hardbreaks, smart, normalize, as.integer(width), extensions)
 }
 
 #' @export
 #' @rdname commonmark
-markdown_latex <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, width = 0){
+markdown_latex <- function(text, hardbreaks = FALSE, smart = FALSE, normalize = FALSE, width = 0, extensions = FALSE){
   text <- enc2utf8(paste(text, collapse="\n"))
-  .Call(R_render_markdown, text, 5L, FALSE, hardbreaks, smart, normalize, as.integer(width))
+  extensions <- get_extensions(extensions)
+  .Call(R_render_markdown, text, 5L, FALSE, hardbreaks, smart, normalize, as.integer(width), extensions)
 }
